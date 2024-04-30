d688478cd02b69d7778a0ce6aa3240d89aafd873
kacper0276 <kacper0276@op.pl>
2024-04-30 14:50:59 +0200

    Dodanie loga (pseudo), ukończona nawigacja strony

--- frontend/src/app/modules/core/core.module.ts
+++ frontend/src/app/modules/core/core.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    HttpClientModule
   ],
   exports:[
     HeaderComponent,
