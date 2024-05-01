d042babeeccca123efd06e9afbdf97126349783c
kacper0276 <kacper0276@op.pl>
2024-05-01 10:14:51 +0200

    Utworzenie modułów posts i userSettings

---
+++ frontend/src/app/modules/user-settings/user-settings.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserSettingsRoutingModule } from './user-settings-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserSettingsRoutingModule
+  ]
+})
+export class UserSettingsModule { }
