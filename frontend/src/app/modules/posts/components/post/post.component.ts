b1c2aabbe6c7e657f99dd45cffd9ca05af3d433f
kacper0276 <kacper0276@op.pl>
2024-05-03 10:20:06 +0200

    Dodanie podstawowego wyświetlania postów, szkielet posts.html

--- frontend/src/app/modules/posts/components/post/post.component.ts
+++ frontend/src/app/modules/posts/components/post/post.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
 export class PostComponent {
-
+  @Input() posts!: {
+    name: string;
+    img: string;
+    description: string;
+    like: number;
+    comments: string[];
+    author: string;
+  };
 }
--- frontend/src/app/modules/posts/components/posts/posts.component.ts
+++ frontend/src/app/modules/posts/components/posts/posts.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss'
+  styleUrl: './posts.component.scss',
 })
 export class PostsComponent {
-
+  firstPost = [
+    {
+      name: 'Nazwa',
+      img: 'zdjecie',
+      description: 'lorem',
+      like: 15,
+      comments: ['Pierwszwy', 'Drugi'],
+      author: 'Kacper Renkel',
+    },
+    {
+      name: 'Nazwa #2',
+      img: 'zdjecie',
+      description: 'lorem ipsum',
+      like: 300,
+      comments: ['Pierwszwy', 'Drugi', 'ósmy', 'komentarz'],
+      author: 'Kacper Renkel',
+    },
+  ];
 }
