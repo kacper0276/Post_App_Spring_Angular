05c9977267224a6905e3d97a71525057a8d08fcb
kacper0276 <kacper0276@op.pl>
2024-05-06 00:31:44 +0200

    Odczytywanie danych przekazanych za pomocą interpolacji w komponencie error i wyświetlenie tego (<ng-content></ng-content>)

--- frontend/src/app/modules/shared/components/error/error.component.ts
+++ frontend/src/app/modules/shared/components/error/error.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrl: './error.component.scss'
+  styleUrl: './error.component.scss',
 })
 export class ErrorComponent {
+  constructor(private elementRef: ElementRef) {}
 
+  ngAfterViewInit() {
+    const content = this.elementRef.nativeElement.textContent.trim();
+    console.log(content);
+  }
 }
