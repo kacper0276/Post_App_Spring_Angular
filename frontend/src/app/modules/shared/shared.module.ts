95fbfc282cf7be95cf7525221aab28d03bd13096
kacper0276 <kacper0276@op.pl>
2024-05-05 08:15:26 +0200

    Utworzenie serwisu do notyfikacji, komponentów do notyfikacji i alert

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SliceTextPipe } from './pipes/slice-text.pipe';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
+import { NotificationComponent } from './components/notification/notification.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
-  declarations: [SliceTextPipe, ClickOutsideDirective],
+  declarations: [
+    SliceTextPipe,
+    ClickOutsideDirective,
+    NotificationComponent,
+    AlertComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     SliceTextPipe,
     ClickOutsideDirective,
+    AlertComponent,
+    NotificationComponent,
   ],
 })
 export class SharedModule {}
