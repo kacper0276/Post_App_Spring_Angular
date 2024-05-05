22e789361b170637bd2707355acc331fbc96301c
kacper0276 <kacper0276@op.pl>
2024-05-05 08:22:27 +0200

    Implementacja logiki komponentu Alert

--- frontend/src/app/modules/shared/components/alert/alert.component.ts
+++ frontend/src/app/modules/shared/components/alert/alert.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrl: './alert.component.scss'
+  styleUrl: './alert.component.scss',
 })
 export class AlertComponent {
+  @Input() message!: string;
+  @Input() typeAlert!: string;
 
+  changeTypeAlertToClassname(): string {
+    switch (this.typeAlert) {
+      case 'info':
+        return 'info-class';
+      case 'danger':
+        return 'danger-class';
+      default:
+        return 'info-class';
+    }
+  }
 }
