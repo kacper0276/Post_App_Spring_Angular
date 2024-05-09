c728c152b5d48ce96062790868a714d333351739
kacper0276 <kacper0276@op.pl>
2024-05-09 08:58:46 +0200

    Podpięcie FormControl oraz FormGroup do formularza zmiany danych uzytkownika

--- frontend/src/app/modules/user-settings/components/change-user-data/change-user-data.component.ts
+++ frontend/src/app/modules/user-settings/components/change-user-data/change-user-data.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormService } from '../../../core/services/form.service';
+import { FormGroup } from '@angular/forms';
+import { ChangeUserDataForm } from '../../../core/models/forms.model';
 
 @Component({
   selector: 'app-change-user-data',
   templateUrl: './change-user-data.component.html',
-  styleUrl: './change-user-data.component.scss'
+  styleUrl: './change-user-data.component.scss',
 })
 export class ChangeUserDataComponent {
+  changeUserDataForm: FormGroup<ChangeUserDataForm> =
+    this.formService.initChangeUserDataForm();
+  constructor(private formService: FormService) {}
 
+  get controls() {
+    return this.changeUserDataForm.controls;
+  }
 }
--- frontend/src/app/modules/user-settings/user-settings.module.ts
+++ frontend/src/app/modules/user-settings/user-settings.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UserSettingsRoutingModule } from './user-settings-routing.module';
 import { ChangeUserDataComponent } from './components/change-user-data/change-user-data.component';
 import { UserSettingsComponent } from './user-settings.component';
 import { EditUserPostsComponent } from './components/edit-user-posts/edit-user-posts.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [UserSettingsComponent, ChangeUserDataComponent, EditUserPostsComponent],
-  imports: [CommonModule, UserSettingsRoutingModule],
+  declarations: [
+    UserSettingsComponent,
+    ChangeUserDataComponent,
+    EditUserPostsComponent,
+  ],
+  imports: [SharedModule, UserSettingsRoutingModule],
 })
 export class UserSettingsModule {}
