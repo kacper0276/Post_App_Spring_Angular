6b5faaad4743d50d8d01f868bb3a1e8f73f714ec
kacper0276 <kacper0276@op.pl>
2024-05-09 10:09:03 +0200

    Rozsuwane menu mobilne dla ustawień

--- frontend/src/app/modules/user-settings/user-settings.component.ts
+++ frontend/src/app/modules/user-settings/user-settings.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html',
-  styleUrl: './user-settings.component.scss'
+  styleUrl: './user-settings.component.scss',
 })
 export class UserSettingsComponent {
+  menuExtend: boolean = false;
 
+  changeMenuVisible(): void {
+    this.menuExtend = !this.menuExtend;
+  }
 }
