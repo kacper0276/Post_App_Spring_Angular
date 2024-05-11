936317753a2454cd44ef66d0335a36b81d7cbdb0
kacper0276 <kacper0276@op.pl>
2024-05-11 12:58:55 +0200

    Domyślne wartości (oryginalne posta), szkielet formularza edycji danych posta

--- frontend/src/app/modules/user-settings/components/user-post-edit-form/user-post-edit-form.component.ts
+++ frontend/src/app/modules/user-settings/components/user-post-edit-form/user-post-edit-form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { test } from '../../../posts/components/posts/posts.component';
 import { firstPost } from '../../../test.db';
+import { FormService } from '../../../core/services/form.service';
+import { FormGroup } from '@angular/forms';
+import { EditPostForm } from '../../../core/models/forms.model';
 
 @Component({
   selector: 'app-user-post-edit-form',
@@ -11,14 +14,34 @@ import { firstPost } from '../../../test.db';
 export class UserPostEditFormComponent implements OnInit {
   id!: number | null;
   post: test | undefined = undefined;
+  editPostForm: FormGroup<EditPostForm> = this.formService.initPostEditForm();
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private formService: FormService
+  ) {}
+
+  get controls() {
+    return this.editPostForm.controls;
+  }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
 
-    // this.post = firstPost.find((post) => post.id == this.id);
+    this.post = firstPost.find((post) => post.id == this.id);
 
-    console.log(this.post);
+    this.editPostForm.setValue({
+      id: this.id,
+      author: 'Kacper Renkel',
+      description: this.post!.description,
+      img: this.post!.img,
+      name: this.post!.name,
+    });
+
+    console.log(this.editPostForm);
+  }
+
+  onChangePostData(): void {
+    console.log(this.editPostForm);
   }
 }
