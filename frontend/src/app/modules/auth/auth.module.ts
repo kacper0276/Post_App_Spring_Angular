831de6c2e60d106db84580bf269383a8562d82e0
kacper0276 <kacper0276@op.pl>
2024-05-13 00:55:21 +0200

    Poprawka w importach w auth.module, i poprawa stylu dla odnośnika w formularzu logowania

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterComponent } from './components/register/register.component';
 import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, RecoverPasswordComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
 })
 export class AuthModule {}
