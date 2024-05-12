f5bc930f02e484b1beac36d40e98c08758f7bbfb
kacper0276 <kacper0276@op.pl>
2024-05-13 00:50:13 +0200

    Podstawowy szkielet formularza do dodawania posta

--- frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
+++ frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FormService } from '../../../core/services/form.service';
+import { FormGroup } from '@angular/forms';
+import { AddPostForm } from '../../../core/models/forms.model';
 
 @Component({
   selector: 'app-add-user-post',
@@ -7,7 +10,17 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './add-user-post.component.scss',
 })
 export class AddUserPostComponent {
-  constructor(private titleService: Title) {
+  addPostForm: FormGroup<AddPostForm> = this.formService.initAddPostForm();
+
+  constructor(private titleService: Title, private formService: FormService) {
     titleService.setTitle('Dodaj artykuł');
+  }
+
+  get controls() {
+    return this.addPostForm.controls;
+  }
+
+  onAddPost(): void {
+    console.log(this.addPostForm);
   }
 }
