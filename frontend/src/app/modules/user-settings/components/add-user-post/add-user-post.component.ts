4a96b941d1ecb16cb2404acdf11711b0521be370
kacper0276 <kacper0276@op.pl>
2024-05-13 00:10:32 +0200

    Ustawienie tytułu dla podstron ustawień

--- frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
+++ frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-user-post',
   templateUrl: './add-user-post.component.html',
-  styleUrl: './add-user-post.component.scss'
+  styleUrl: './add-user-post.component.scss',
 })
 export class AddUserPostComponent {
-
+  constructor(private titleService: Title) {
+    titleService.setTitle('Dodaj artykuł');
+  }
 }
