e83b0a732fe6c1ae226ad718cba24dc7b841c0c6
kacper0276 <kacper0276@op.pl>
2024-06-11 16:41:09 +0200

    [BE] Create user Entity

---
+++ backend/src/main/java/kacperrenkel/postapp/backend/entity/User.java
@@ -0,0 +1,23 @@
+package kacperrenkel.postapp.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "uzytkownicy")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+    private String username;
+    private String password;
+}
