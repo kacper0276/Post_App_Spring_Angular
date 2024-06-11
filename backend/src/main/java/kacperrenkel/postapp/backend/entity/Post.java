900ac05ed17ac1160e49a8f7d0e0794c69852ac2
kacper0276 <kacper0276@op.pl>
2024-06-12 01:32:14 +0200

    [BE] Create Post entity

---
+++ backend/src/main/java/kacperrenkel/postapp/backend/entity/Post.java
@@ -0,0 +1,30 @@
+package kacperrenkel.postapp.backend.entity;
+
+import jakarta.persistence.*;
+import kacperrenkel.postapp.backend.util.StringListConverter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "posty")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String content;
+    private String author;
+    private int likes;
+    @Convert(converter = StringListConverter.class)
+    private List<String> comments;
+    private String image;
+}
