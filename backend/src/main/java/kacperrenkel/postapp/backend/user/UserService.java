0efbf78ae466e5177223a304ac7746c47c3c36d0
kacper0276 <kacper0276@op.pl>
2024-06-14 17:22:03 +0200

    [BE] Create SecurityConfiguration

--- backend/src/main/java/kacperrenkel/postapp/backend/user/UserService.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/user/UserService.java
@@ -1,10 +1,12 @@
 package kacperrenkel.postapp.backend.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 }
