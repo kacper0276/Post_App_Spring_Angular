7ec9ba1bbf8c88556fe8235ca611a0749ac0a640
kacper0276 <kacper0276@op.pl>
2024-06-14 22:01:36 +0200

    [BE] Security Config (Protected Route)

--- backend/src/main/java/kacperrenkel/postapp/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/configuration/SecurityConfiguration.java
@@ -2,13 +2,30 @@ package kacperrenkel.postapp.backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/users/rejestruj", "users/zaloguj").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
