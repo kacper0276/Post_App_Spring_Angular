c2badd0371811c44a380d4d750c1fe7baa00e7c3
kacper0276 <kacper0276@op.pl>
2024-06-15 03:00:54 +0200

    [FE] Fix in show passwords buttons

--- frontend/src/app/modules/auth/components/register/register.component.ts
+++ frontend/src/app/modules/auth/components/register/register.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { FormService } from '../../../core/services/form.service';
 import { FormGroup } from '@angular/forms';
 import { RegisterForm } from '../../../core/models/forms.model';
 import { Title } from '@angular/platform-browser';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -12,21 +13,34 @@ import { Title } from '@angular/platform-browser';
 export class RegisterComponent {
   registerForm: FormGroup<RegisterForm> = this.formService.initRegisterForm();
   alertMsg: string | null = null;
-  @ViewChild('passwordInput') inputPassword!: ElementRef;
+
+  @ViewChildren('passwordInput') inputPasswords!: QueryList<ElementRef>;
+
   get controls() {
     return this.registerForm.controls;
   }
 
-  constructor(private formService: FormService, private titleService: Title) {
+  constructor(
+    private formService: FormService,
+    private authService: AuthService,
+    private titleService: Title
+  ) {
     titleService.setTitle('Rejestracja');
   }
 
   onRegister() {
     console.log(this.registerForm.getRawValue());
+    const { email, password, username } = this.registerForm.getRawValue();
+
+    this.authService
+      .register({ email, username, password })
+      .subscribe((val) => console.log(val));
   }
 
-  changeVisiblePassword(): void {
-    this.inputPassword.nativeElement.type =
-      this.inputPassword.nativeElement.type == 'password' ? 'text' : 'password';
+  changeVisiblePassword(e: Event, index: number): void {
+    e.preventDefault();
+    const passwordInput = this.inputPasswords.toArray()[index].nativeElement;
+    passwordInput.type =
+      passwordInput.type === 'password' ? 'text' : 'password';
   }
 }
