3c680634209b28d8b29928fb03d2576624ec920d
kacper0276 <kacper0276@op.pl>
2024-06-17 08:56:11 +0200

    [FE] Create AdminGuard

--- frontend/src/app/modules/core/guards/admin.guard.ts
+++ frontend/src/app/modules/core/guards/admin.guard.ts
@@ -1,5 +1,36 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../store/app.reducer';
+import { catchError, map, of, switchMap, take } from 'rxjs';
+import { selectAuthUser } from '../../auth/store/auth.selectors';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const store = inject(Store<AppState>);
+
+  return authService.isLoggedIn().pipe(
+    take(1),
+    switchMap((resp) => {
+      const isLoggedIn = resp.message;
+
+      if (isLoggedIn) {
+        return store.select(selectAuthUser).pipe(
+          map((user) => {
+            if (user && user.role === 'Administrator') {
+              return true;
+            }
+
+            return false;
+          })
+        );
+      }
+
+      return of(false);
+    }),
+    catchError((err) => {
+      return of(false);
+    })
+  );
 };
