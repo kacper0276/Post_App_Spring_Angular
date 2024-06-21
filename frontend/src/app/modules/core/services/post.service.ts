fbb15db741bc516441a7a7a0726ae75eb2efedb0
kacper0276 <kacper0276@op.pl>
2024-06-21 15:41:40 +0200

    [FE] Create post service

---
+++ frontend/src/app/modules/core/services/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../../environment/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IPost } from '../models/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  apiUrl = `${environment.apiUrl}`;
+
+  constructor(private http: HttpClient) {}
+
+  public getAllPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(`${this.apiUrl}/posts/`);
+  }
+}
