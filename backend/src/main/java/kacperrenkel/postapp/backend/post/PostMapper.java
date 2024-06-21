8918b84ef4feee2c863d095c84782baf7091299d
kacper0276 <kacper0276@op.pl>
2024-06-21 19:28:04 +0200

    [BE] Create mapper

---
+++ backend/src/main/java/kacperrenkel/postapp/backend/post/PostMapper.java
@@ -0,0 +1,13 @@
+package kacperrenkel.postapp.backend.post;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PostMapper {
+    PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
+
+    @Mapping(target = "userId", source = "post.user.id")
+    PostDTO postToPostDto(Post post);
+}
