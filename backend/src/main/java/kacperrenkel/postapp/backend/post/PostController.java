220f75880c71902fc195623d8b9f94bca68ac39a
kacper0276 <kacper0276@op.pl>
2024-06-21 10:33:54 +0200

    [BE] Create method to get all posts

--- backend/src/main/java/kacperrenkel/postapp/backend/post/PostController.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/post/PostController.java
@@ -1,13 +1,21 @@
 package kacperrenkel.postapp.backend.post;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/posts")
 @AllArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @GetMapping(path = "/")
+    public ResponseEntity<List<Post>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
 }
--- backend/src/main/java/kacperrenkel/postapp/backend/post/PostService.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/post/PostService.java
@@ -3,8 +3,14 @@ package kacperrenkel.postapp.backend.post;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+
+    public List<Post> getAllPosts(){
+        return postRepository.findAll();
+    }
 }
