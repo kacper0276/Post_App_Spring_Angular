a7a12497e5e14d02da6d6928ccba6fbe836ca7ae
kacper0276 <kacper0276@op.pl>
2024-06-23 09:41:52 +0200

    [BE] Add new endpoint in security

--- backend/src/main/java/kacperrenkel/postapp/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/configuration/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "users/register", "users/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "users/auto-login", "users/logout", "users/logged-in", "posts/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "users/auto-login", "users/logout", "users/logged-in", "users/activate", "posts/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
