e25725b7db809b477da9fd9ab83a448a918d97e7
kacper0276 <kacper0276@op.pl>
2024-06-24 09:44:50 +0200

    [FE] Basic config notifier

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,22 @@ import { SharedModule } from './modules/shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { authReducer } from './modules/auth/store/auth.reducer';
 import { AuthEffects } from './modules/auth/store/auth.effects';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+
+const customNotifier: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'right',
+      distance: 12,
+    },
+    vertical: {
+      position: 'top',
+      distance: 12,
+      gap: 10,
+    },
+  },
+  theme: 'material',
+};
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
@@ -24,6 +40,7 @@ import { AuthEffects } from './modules/auth/store/auth.effects';
     StoreModule.forRoot({ auth: authReducer }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([AuthEffects]),
+    NotifierModule.withConfig(customNotifier),
   ],
   providers: [],
   bootstrap: [AppComponent],
