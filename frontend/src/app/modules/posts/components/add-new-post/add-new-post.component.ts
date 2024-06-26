a2e911bf0fcac6030f29124e8b37409dd857fff0
kacper0276 <kacper0276@op.pl>
2024-06-26 08:32:26 +0200

    [FE] Init form in add new post component

--- frontend/src/app/modules/posts/components/add-new-post/add-new-post.component.ts
+++ frontend/src/app/modules/posts/components/add-new-post/add-new-post.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormService } from '../../../core/services/form.service';
+import { FormGroup } from '@angular/forms';
+import { AddPostForm } from '../../../core/models/forms.model';
 
 @Component({
   selector: 'app-add-new-post',
   templateUrl: './add-new-post.component.html',
-  styleUrl: './add-new-post.component.scss'
+  styleUrl: './add-new-post.component.scss',
 })
 export class AddNewPostComponent {
+  addPostForm: FormGroup<AddPostForm> = this.formService.initAddPostForm();
 
+  get controls() {
+    return this.addPostForm.controls;
+  }
+
+  constructor(private formService: FormService) {}
 }
