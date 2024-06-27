1411f1b441fa5c7644a6f226cd42951263ad7e0d
kacper0276 <kacper0276@op.pl>
2024-06-28 01:01:07 +0200

    [FE] Changes in add-user-post

--- frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
+++ frontend/src/app/modules/user-settings/components/add-user-post/add-user-post.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { FormService } from '../../../core/services/form.service';
 import { FormGroup } from '@angular/forms';
 import { AddPostForm } from '../../../core/models/forms.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../../store/app.reducer';
+import { selectAuthUser } from '../../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-add-user-post',
   templateUrl: './add-user-post.component.html',
   styleUrl: './add-user-post.component.scss',
 })
-export class AddUserPostComponent {
+export class AddUserPostComponent implements OnInit {
   addPostForm: FormGroup<AddPostForm> = this.formService.initAddPostForm();
-
-  constructor(private titleService: Title, private formService: FormService) {
-    titleService.setTitle('Dodaj artykuł');
-  }
 
   get controls() {
     return this.addPostForm.controls;
+  }
+
+  constructor(
+    private titleService: Title,
+    private formService: FormService,
+    private store: Store<AppState>
+  ) {
+    titleService.setTitle('Dodaj artykuł');
+  }
+
+  ngOnInit(): void {
+    this.store.select(selectAuthUser).subscribe({
+      next: (val: any) => {
+        this.controls['author'].setValue(val.username);
+      },
+    });
   }
 
   onAddPost(): void {
