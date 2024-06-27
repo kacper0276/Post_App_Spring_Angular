9c6c6c80c8ce2abec9bc5094cef73995cc0c1441
kacper0276 <kacper0276@op.pl>
2024-06-28 00:39:07 +0200

    [FE] Create form for create post

--- frontend/src/app/modules/posts/components/add-new-post/add-new-post.component.ts
+++ frontend/src/app/modules/posts/components/add-new-post/add-new-post.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormService } from '../../../core/services/form.service';
 import { FormGroup } from '@angular/forms';
 import { AddPostForm } from '../../../core/models/forms.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../../store/app.reducer';
+import { selectAuthUser } from '../../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-add-new-post',
   templateUrl: './add-new-post.component.html',
   styleUrl: './add-new-post.component.scss',
 })
-export class AddNewPostComponent {
+export class AddNewPostComponent implements OnInit {
   addPostForm: FormGroup<AddPostForm> = this.formService.initAddPostForm();
 
   get controls() {
     return this.addPostForm.controls;
   }
 
-  constructor(private formService: FormService) {}
+  constructor(
+    private formService: FormService,
+    private store: Store<AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this.store.select(selectAuthUser).subscribe({
+      next: (val: any) => {
+        this.controls['author'].setValue(val.username);
+      },
+    });
+  }
 
   onAddNewPost(): void {
     console.log('TEST');
