384baa8ba6c8e55d88473bd356c20fbdee0981e9
kacper0276 <kacper0276@op.pl>
2024-07-06 02:19:17 +0200

    [FE] Implements add comment in comments component

--- frontend/src/app/modules/posts/components/comments-component/comments-component.component.ts
+++ frontend/src/app/modules/posts/components/comments-component/comments-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { FormService } from '../../../core/services/form.service';
+import { FormGroup } from '@angular/forms';
+import { AddComment } from '../../../core/models/forms.model';
 
 @Component({
   selector: 'app-comments-component',
@@ -6,5 +9,16 @@ import { Component, Input } from '@angular/core';
   styleUrl: './comments-component.component.scss',
 })
 export class CommentsComponentComponent {
+  addCommentForm: FormGroup<AddComment> = this.formService.initAddCommentForm();
   @Input() comments!: string[];
+
+  get controls() {
+    return this.addCommentForm.controls;
+  }
+
+  constructor(private formService: FormService) {}
+
+  onAddComment(): void {
+    console.log(this.controls.comment);
+  }
 }
