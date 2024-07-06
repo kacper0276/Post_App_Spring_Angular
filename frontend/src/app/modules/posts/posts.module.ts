805cf637106fd1eb28aa37dfab1e9bf25c5f004f
kacper0276 <kacper0276@op.pl>
2024-07-07 01:07:56 +0200

    [FE] Change using comments component in posts module

--- frontend/src/app/modules/posts/posts.module.ts
+++ frontend/src/app/modules/posts/posts.module.ts
@@ -5,18 +5,17 @@ import { PostsComponent } from './components/posts/posts.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { PostComponent } from './components/post/post.component';
 import { SharedModule } from '../shared/shared.module';
-import { CommentsComponentComponent } from './components/comments-component/comments-component.component';
 import { CoreModule } from '../core/core.module';
 import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
+import { CommentsModule } from '../comments/comments.module';
 
 @NgModule({
   declarations: [
     PostsComponent,
     PostDetailsComponent,
     PostComponent,
-    CommentsComponentComponent,
     AddNewPostComponent,
   ],
-  imports: [SharedModule, PostsRoutingModule, CoreModule],
+  imports: [SharedModule, PostsRoutingModule, CoreModule, CommentsModule],
 })
 export class PostsModule {}
