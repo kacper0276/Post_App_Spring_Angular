3d17695c4b23a1672b4cc20ed575ab1cafcdb609
kacper0276 <kacper0276@op.pl>
2024-07-07 01:31:09 +0200

    [FE] Create comment component

--- frontend/src/app/modules/comments/comments.module.ts
+++ frontend/src/app/modules/comments/comments.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommentComponent } from './components/comment/comment.component';
 import { SharedModule } from '../shared/shared.module';
 import { CommentsComponent } from './components/comments/comments.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [CommentComponent, CommentsComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, RouterLink],
   exports: [CommentComponent, CommentsComponent],
 })
 export class CommentsModule {}
--- frontend/src/app/modules/comments/components/comment/comment.component.ts
+++ frontend/src/app/modules/comments/components/comment/comment.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IComment } from '../../../core/models/comment.model';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.scss'
+  styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
-
+  @Input() comment!: IComment;
 }
