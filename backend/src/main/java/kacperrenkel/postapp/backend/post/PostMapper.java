493b2d7b9140f19cbbd3c572ebfa709476804bf2
kacper0276 <kacper0276@op.pl>
2024-07-06 10:21:37 +0200

    [BE] Change mapper for post

--- backend/src/main/java/kacperrenkel/postapp/backend/post/PostMapper.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/post/PostMapper.java
@@ -1,8 +1,14 @@
 package kacperrenkel.postapp.backend.post;
 
+import kacperrenkel.postapp.backend.comment.Comment;
+import kacperrenkel.postapp.backend.comment.CommentDTO;
+import kacperrenkel.postapp.backend.comment.CommentMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper
 public interface PostMapper {
@@ -10,5 +16,12 @@ public interface PostMapper {
 
     @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "userProfileImage", source = "user.image")
+    @Mapping(target = "comments", expression = "java(mapComments(post.getComments()))")
     PostDTO postToPostDto(Post post);
+
+    default List<CommentDTO> mapComments(List<Comment> comments) {
+        return comments.stream()
+                .map(CommentMapper.INSTANCE::commentToCommentDto)
+                .collect(Collectors.toList());
+    }
 }
