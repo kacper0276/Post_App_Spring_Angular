8952caa4f4207c4778a9802eb991e4ecec1a849f
kacper0276 <kacper0276@op.pl>
2024-07-07 01:36:28 +0200

    [FE] Show if user add like in post details

--- frontend/src/app/modules/posts/components/post-details/post-details.component.ts
+++ frontend/src/app/modules/posts/components/post-details/post-details.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { test } from '../../../core/models/test.model';
+import { Component, Input, OnInit } from '@angular/core';
 import { IPost } from '../../../core/models/post.model';
+import { AppState } from '../../../../../store/app.reducer';
+import { Store } from '@ngrx/store';
+import { selectAuthUser } from '../../../auth/store/auth.selectors';
+import { IUser } from '../../../core/models/auth.model';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.scss',
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
   @Input() post!: IPost;
   showComments: boolean = false;
+  userGiveLike: boolean = false;
+  username = '';
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.select(selectAuthUser).subscribe({
+      next: (val: IUser | null) => {
+        val != null ? (this.username = val.username) : (this.username = '');
+
+        if (val?.likes.includes(String(this.post.id))) {
+          this.userGiveLike = true;
+        }
+      },
+    });
+  }
 
   showCommentFunction(): void {
     this.showComments = true;
