18e787444c112a86f4ca127586a663b45c0c23de
kacper0276 <kacper0276@op.pl>
2024-07-10 22:06:39 +0200

    [FE] Create chat list user

--- frontend/src/app/modules/chat/chat.module.ts
+++ frontend/src/app/modules/chat/chat.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatsUserComponent } from './components/chats-user/chats-user.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ChatListComponentComponent } from './components/chat-list-component/chat-list-component.component';
 
 
 @NgModule({
   declarations: [
     ChatsUserComponent,
-    ChatComponent
+    ChatComponent,
+    ChatListComponent,
+    ChatListComponentComponent
   ],
   imports: [
     CommonModule,
---
+++ frontend/src/app/modules/chat/components/chat-list/chat-list.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-list',
+  templateUrl: './chat-list.component.html',
+  styleUrl: './chat-list.component.scss',
+})
+export class ChatListComponent {
+  arr: string[] = [
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+  ];
+}
