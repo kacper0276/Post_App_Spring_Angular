f7156c4014669c8b0abb2692baba51913bd23b56
kacper0276 <kacper0276@op.pl>
2024-07-17 04:42:13 +0200

    [FE] Basic configuration for translate

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.reducer';
 import * as AuthActions from '../app/modules/auth/store/auth.actions';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,14 @@ import * as AuthActions from '../app/modules/auth/store/auth.actions';
 export class AppComponent {
   title = 'frontend';
 
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private translate: TranslateService
+  ) {
+    translate.setDefaultLang('en');
+
+    translate.use('en');
+  }
 
   ngOnInit(): void {
     this.store.dispatch(AuthActions.autoLogin());
