24264dfa1c23a06717d92bd7a09b5656e3e2a359
kacper0276 <kacper0276@op.pl>
2024-07-20 19:20:52 +0200

    [FE] Basic style for user-profile-component

--- frontend/src/app/modules/user-profile/components/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/modules/user-profile/components/user-profile-page/user-profile-page.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IUser } from '../../../core/models/auth.model';
+import { UserProfileService } from '../../../core/services/user-profile.service';
+import { IPost } from '../../../core/models/post.model';
 
 @Component({
   selector: 'app-user-profile-page',
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.scss',
 })
-export class UserProfilePageComponent {
+export class UserProfilePageComponent implements OnInit {
   username!: string | null;
+  userData!: IUser | null;
+  posts: IPost[] | null = null;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private readonly userProfileService: UserProfileService
+  ) {
     this.username = route.snapshot.paramMap.get('username');
+  }
+
+  ngOnInit(): void {
+    if (this.username !== null) {
+      this.userProfileService.getUserData(this.username).subscribe({
+        next: (res: IUser) => {
+          this.userData = res;
+        },
+      });
+    }
   }
 }
--- frontend/src/app/modules/user-profile/user-profile.module.ts
+++ frontend/src/app/modules/user-profile/user-profile.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfilePageComponent } from './components/user-profile-page/user-profile-page.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    UserProfilePageComponent
-  ],
-  imports: [
-    CommonModule,
-    UserProfileRoutingModule
-  ]
+  declarations: [UserProfilePageComponent],
+  imports: [SharedModule, UserProfileRoutingModule],
 })
-export class UserProfileModule { }
+export class UserProfileModule {}
