9e0ca67421682f78a04bb6eb2a580d4747f2c28f
kacper0276 <kacper0276@op.pl>
2024-07-22 22:57:00 +0200

    [FE] Replace username to IUser Data

--- frontend/src/app/modules/chat/components/chat-list-component/chat-list-component.component.ts
+++ frontend/src/app/modules/chat/components/chat-list-component/chat-list-component.component.ts
@@ -1,25 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IUser } from '../../../core/models/auth.model';
+import { UserProfileService } from '../../../core/services/user-profile.service';
 import { IMessage } from '../../../core/models/message.model';
 
 @Component({
   selector: 'app-chat-list-component',
   templateUrl: './chat-list-component.component.html',
-  styleUrl: './chat-list-component.component.scss',
+  styleUrls: ['./chat-list-component.component.scss'],
 })
 export class ChatListComponentComponent implements OnInit {
   @Input() username!: string;
   @Input() message!: IMessage;
-  otherUsername!: string;
+  otherUser!: IUser;
+
+  constructor(private userProfileService: UserProfileService) {}
 
   ngOnInit(): void {
-    this.otherUsername = this.getOtherUsername();
+    this.getUsername();
   }
 
-  getOtherUsername(): string {
-    if (this.message.messageFromUsername !== this.username) {
-      return this.message.messageFromUsername;
-    } else {
-      return this.message.messageToUsername;
+  async getUsername() {
+    try {
+      const user: IUser = await this.getOtherUsername();
+      this.otherUser = user;
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  getOtherUsername(): Promise<IUser> {
+    let username = '';
+    if (this.message.messageFromUsername !== this.username) {
+      username = this.message.messageFromUsername;
+    } else {
+      username = this.message.messageToUsername;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.userProfileService.getUserData(username).subscribe({
+        next: (val: IUser) => {
+          resolve(val);
+        },
+        error: (err) => {
+          reject(err);
+        },
+      });
+    });
   }
 }
