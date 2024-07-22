b2eb78d7dbcb7577df35e5ffb6ec841f674d7b7d
kacper0276 <kacper0276@op.pl>
2024-07-22 03:27:58 +0200

    [BE] Create methods to get and set messages

--- backend/src/main/java/kacperrenkel/postapp/backend/message/MessageController.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/message/MessageController.java
@@ -1,12 +1,35 @@
 package kacperrenkel.postapp.backend.message;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/message")
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageService messageService;
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(Message message, SimpMessageHeaderAccessor headerAccessor) {
+        System.out.println("Received message: " + message);
+        message.setTime(new Date());
+        return messageService.saveMessage(message);
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessagesBetweenUsers(
+            @RequestParam String user1,
+            @RequestParam String user2) {
+        return messageService.getMessagesBetweenUsers(user1, user2);
+    }
 }
