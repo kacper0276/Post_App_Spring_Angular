946fccc17026ac060e32016b50eace6ed2a3e5bc
kacper0276 <kacper0276@op.pl>
2024-07-27 10:17:46 +0200

    [BE] Implements new method for mapping in PostMapper

--- backend/src/main/java/kacperrenkel/postapp/backend/post/PostMapper.java
+++ backend/src/main/java/kacperrenkel/postapp/backend/post/PostMapper.java
@@ -3,9 +3,11 @@ package kacperrenkel.postapp.backend.post;
 import kacperrenkel.postapp.backend.comment.Comment;
 import kacperrenkel.postapp.backend.comment.CommentDTO;
 import kacperrenkel.postapp.backend.comment.CommentMapper;
+import kacperrenkel.postapp.backend.entity.PaginatedResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +25,19 @@ public interface PostMapper {
         return comments.stream()
                 .map(CommentMapper.INSTANCE::commentToCommentDto)
                 .collect(Collectors.toList());
+    }
+
+    default PaginatedResponse<PostDTO> pageToPaginatedResponse(Page<Post> postPage) {
+        List<PostDTO> postDTOs = postPage.stream()
+                .map(this::postToPostDto)
+                .collect(Collectors.toList());
+
+        PaginatedResponse<PostDTO> response = new PaginatedResponse<>();
+        response.setContent(postDTOs);
+        response.setCurrentPage(postPage.getNumber());
+        response.setTotalPages(postPage.getTotalPages());
+        response.setTotalItems(postPage.getTotalElements());
+
+        return response;
     }
 }
