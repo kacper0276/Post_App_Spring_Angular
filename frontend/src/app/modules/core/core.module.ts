19b5b6ca5c5207a337601c3beb664b90f9300370
kacper0276 <kacper0276@op.pl>
2024-07-31 06:26:57 +0200

    [FE] Create service for spinner

--- frontend/src/app/modules/core/core.module.ts
+++ frontend/src/app/modules/core/core.module.ts
@@ -5,9 +5,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NavbarComponent],
+  declarations: [HeaderComponent, FooterComponent, NavbarComponent, SpinnerComponent],
   imports: [SharedModule, RouterLink, RouterLinkActive, HttpClientModule],
   exports: [HeaderComponent, FooterComponent],
 })
