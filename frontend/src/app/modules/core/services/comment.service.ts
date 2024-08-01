6e17fd2c3444dd5e2240f307ed6ce3ac06001f53
kacper0276 <kacper0276@op.pl>
2024-08-01 03:10:34 +0200

    [FE] Create methods CRUD for comments in service comment

--- frontend/src/app/modules/core/services/comment.service.ts
+++ frontend/src/app/modules/core/services/comment.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IComment } from '../models/comment.model';
@@ -14,5 +14,29 @@ export class CommentService {
 
   public getCommentsInPost(postId: number): Observable<IComment[]> {
     return this.http.get<IComment[]>(`${this.apiUrl}/comments/${postId}`);
+  }
+
+  addComment(
+    username: string,
+    postId: number,
+    comment: string
+  ): Observable<any> {
+    const params = new HttpParams()
+      .append('username', username)
+      .append('postId', postId);
+
+    return this.http.post<any>(`${this.apiUrl}/comments/add`, comment, {
+      params,
+    });
+  }
+
+  deleteComment(commentId: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/comments/delete/${commentId}`);
+  }
+
+  editComment(commentId: number, newContent: string): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/comments/edit/${commentId}`, {
+      newContent,
+    });
   }
 }
