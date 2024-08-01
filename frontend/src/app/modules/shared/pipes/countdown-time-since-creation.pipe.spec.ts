1c78466fc35a09e8fbe90f32f90631ea4a022476
kacper0276 <kacper0276@op.pl>
2024-08-02 00:39:27 +0200

    [FE] Remove testing files in shared

--- frontend/src/app/modules/shared/pipes/countdown-time-since-creation.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountdownTimeSinceCreationPipe } from './countdown-time-since-creation.pipe';
-
-describe('CountdownTimeSinceCreationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountdownTimeSinceCreationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- frontend/src/app/modules/shared/pipes/slice-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SliceTextPipe } from './slice-text.pipe';
-
-describe('SliceTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SliceTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
