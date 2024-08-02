2e3e3caa28865bba1b0ce55b1e683c7d25e105f3
kacper0276 <kacper0276@op.pl>
2024-08-02 06:46:08 +0200

    [FE] Implement translator in countdown pipe

--- frontend/src/app/modules/shared/pipes/countdown-time-since-creation.pipe.ts
+++ frontend/src/app/modules/shared/pipes/countdown-time-since-creation.pipe.ts
@@ -1,9 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
   name: 'countdownTimeSinceCreation',
 })
 export class CountdownTimeSinceCreationPipe implements PipeTransform {
+  constructor(private translate: TranslateService) {}
+
   transform(value: Date, ...args: unknown[]): string {
     const now = new Date();
     const past = new Date(value);
@@ -14,11 +17,11 @@ export class CountdownTimeSinceCreationPipe implements PipeTransform {
     const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
 
     if (diffInDays >= 1) {
-      return `${diffInDays} dni temu`;
+      return `${diffInDays} ${this.translate.instant('days-ago')}`;
     } else if (diffInHours >= 1) {
-      return `${diffInHours} godzin temu`;
+      return `${diffInHours} ${this.translate.instant('hours-ago')}`;
     } else {
-      return `${diffInMinutes} minut temu`;
+      return `${diffInMinutes} ${this.translate.instant('minutes-ago')}`;
     }
   }
 }
